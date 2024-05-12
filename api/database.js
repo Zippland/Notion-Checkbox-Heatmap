2a2ee9571296e187524d843313a420ee1e505e21
Zylan <126135306+Zippland@users.noreply.github.com>
2024-05-13 03:35:57 +1000

    Update database.js

--- api/database.js
+++ api/database.js
@@ -5,6 +5,7 @@ dotenv.config();
 export default async (req, res) => {
     const token = process.env.ENV_NOTION_TOKEN;
     const databaseId = process.env.ENV_DATABASE_ID;
+    const checkboxPropertyName = process.env.ENV_CHECKBOX_PROPERTY_NAME;
 
     try {
         const response = await fetch(`https://api.notion.com/v1/databases/${databaseId}/query`, {
@@ -21,7 +22,7 @@ export default async (req, res) => {
             throw new Error(`Notion API error: ${response.status} ${JSON.stringify(data)}`);
         }
 
-        const processedData = processData(data.results);
+        const processedData = processData(data.results, checkboxPropertyName);
         res.json(processedData);
     } catch (error) {
         console.error("Error processing request:", error);
@@ -29,20 +30,22 @@ export default async (req, res) => {
     }
 };
 
-const processData = (data) => {
-    const progressMap = new Map();
+const processData = (data, checkboxPropertyName) => {
+    const checkboxMap = {
+        checked: 0,
+        unchecked: 0
+    };
 
     data.forEach(item => {
-        if (item.properties.Date && item.properties.Progress) {
-            if (item.properties.Progress.formula.number !== null && item.properties.Progress.formula.number > 0) {
-                const dateObject = new Date(item.properties.Date.created_time);
-                dateObject.setDate(dateObject.getDate() + 1); // Add one day to the date
-                const date = dateObject.toISOString().split('T')[0]; // Format back to YYYY-MM-DD
-                const progress = Math.round(item.properties.Progress.formula.number * 100); // Convert from 0-1 to 0-100
-                progressMap.set(date, progress);
+        const checkboxValue = item.properties[checkboxPropertyName]?.checkbox;
+        if (checkboxValue !== undefined) {
+            if (checkboxValue) {
+                checkboxMap.checked += 1;
+            } else {
+                checkboxMap.unchecked += 1;
             }
         }
     });
 
-    return Array.from(progressMap).map(([date, progress]) => ({ date, progress }));
+    return checkboxMap;
 };
